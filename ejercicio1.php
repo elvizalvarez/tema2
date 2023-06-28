69ca3ee2073810220e238039b97d848b2c7951a6
elvisalvarez <elvizronaldalvarez@gmail.com>
2023-06-28 10:58:46 +0200

    ejercicio 1

---
+++ ejercicio1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tema2</title>
+</head>
+<body>
+    <?php
+//integer, double, string i boolean.
+
+$numeroInt = 32;  //variable integer
+$numeroDouble = 23.32; // variable //double
+$nombre = "elviz"; //variable string
+$salida = true; // variable boolean
+
+echo $numeroInt; 
+echo "<br>";
+echo $numeroDouble;
+echo "<br>";
+echo $nombre;
+echo "<br>";
+echo $salida;
+
+
+
+
+
+
+
+?>
+
+
+</body>
+</html>
